b6214dc59f3854e2119dc538ba4b299fbe6f35e9
student <student@Carins-MacBook-Pro.local>
2024-04-06 15:52:43 -0500

    Express server initial listening/port 3000

---
+++ server/index.ts
@@ -0,0 +1,13 @@
+// const express = require('express')
+import * as express from "express" ;
+import { Express, Request, Response} from "express"
+const app = express()
+const port = 3000
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
