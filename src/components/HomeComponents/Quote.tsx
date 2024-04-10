99e523e2a1c809afc09efad9b93c9c9814f9e4a1
peytono <pcopcoeco@gmail.com>
2024-04-10 17:01:21 -0500

    add new quote button

--- src/components/HomeComponents/Quote.tsx
+++ src/components/HomeComponents/Quote.tsx
@@ -11,16 +11,22 @@ function Quote() {
 
   const [quote, setQuote] = useState({} as Quote);
 
-  useEffect(() => {
+  const getQuote = () => {
     axios.get('/quotes')
       .then(({ data }: { data: { author: String, quote: String } }) => {
-        console.log(data);
         setQuote(data);
       })
       .catch((err) => console.error('failed getting quote', err));
+  };
+
+  useEffect(() => {
+    getQuote();
   }, []);
   return (
-    <h3>{`${quote.quote} -${quote.author}`}</h3>
+    <>
+      <h3>{`${quote.quote} -${quote.author}`}</h3>
+      <button type="button" onClick={getQuote}>New Quote</button>
+    </>
   );
 }
 
