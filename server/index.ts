3c6f22933d8cf6ea5674f9519106332d214c18ed
peytono <pcopcoeco@gmail.com>
2024-04-10 16:25:14 -0500

    add quotes router w/ single GET

--- server/index.ts
+++ server/index.ts
@@ -10,6 +10,7 @@ require('dotenv').config();
 const app = express();
 const port = 3000;
 const authRouter = require('./routes/auth');
+const quotesRouter = require('./routes/quotes');
 
 app.use(express.json());
 
@@ -31,6 +32,7 @@ const CLIENT_PATH = path.resolve(__dirname, '../dist');
 app.use(express.static(CLIENT_PATH));
 
 app.use('/auth', authRouter);
+app.use('/quotes', quotesRouter);
 
 const checkAuth = (
   req: { isAuthenticated: Function },
---
+++ server/routes/quotes.ts
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const router = express.Router();
+
+const axios = require('axios');
+
+function getQuote() {
+  const { NINJA_API_KEY } = process.env;
+  const quoteUrl = 'https://api.api-ninjas.com/v1/quotes?category=death';
+  return axios.get(quoteUrl, { headers: { 'X-Api-Key': NINJA_API_KEY, 'X-Requested-With': 'XMLHttpRequest' } })
+    .then((response: Response) => response)
+    .catch((err: Error) => console.error('failed getting quote', err));
+}
+
+router.get('/', (req: Request, res: { send: Function, sendStatus: Function }) => {
+  // type AxiosResponse = typeof AxiosResponse;
+  getQuote()
+    .then(({ data }: { data: [ { quote: String, author: String, category: String } ] }) => {
+      res.send(data[0]);
+    })
+    .catch((err: Error) => {
+      console.error('failed getting quote', err);
+      res.sendStatus(500);
+    });
+});
+
+module.exports = router;
