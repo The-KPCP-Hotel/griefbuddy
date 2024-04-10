92f34442a3fe9c6da12e7654740e0edb1b0239a2
peytono <pcopcoeco@gmail.com>
2024-04-10 14:35:42 -0500

    fix home links

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -1,14 +1,13 @@
-import React = require("react");
-import { Link } from "react-router-dom";
+import React = require('react');
+import { Link } from 'react-router-dom';
 
 function Events() {
-
-    return (
-        <div>
-            <Link to="/">Home</Link>
-            <h1>Events Page</h1>
-        </div>
-    )
+  return (
+    <div>
+      <Link to="/home">Home</Link>
+      <h1>Events Page</h1>
+    </div>
+  );
 }
 
-export default Events
+export default Events;
--- src/components/Resources.tsx
+++ src/components/Resources.tsx
@@ -1,14 +1,13 @@
-import React = require("react");
-import { Link } from "react-router-dom";
+import React = require('react');
+import { Link } from 'react-router-dom';
 
 function Resources() {
-
-    return (
-        <div>
-            <Link to="/">Home</Link>
-            <h1>Resources</h1>
-        </div>
-    )
+  return (
+    <div>
+      <Link to="/home">Home</Link>
+      <h1>Resources</h1>
+    </div>
+  );
 }
 
-export default Resources
+export default Resources;
