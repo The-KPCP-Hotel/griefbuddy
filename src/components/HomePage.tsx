9a0ec25d4463dd282422aedcaad5980feaae1ce1
peytono <pcopcoeco@gmail.com>
2024-04-10 14:42:34 -0500

    clean up code

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -10,31 +10,23 @@ function HomePage() {
 
   const { setUser, user } = userContext;
 
+  // want to find a better solution than calling db every time homepage is rendered
   useEffect(() => {
     axios
       .get('/user')
       .then(({ data }: { data: AuthUser }) => {
-        console.log(data);
         if (typeof data === 'object') {
           const curUser = { ...data };
           setUser(curUser);
         }
-        // console.log(user);
       })
       .catch((err: Error) => console.error('failed setting user', err));
-
-    // setUser({
-    //   id: 1,
-    //   name: 'Peyton',
-    //   googleId: '1234',
-    // });
   }, [setUser]);
 
-  console.log(userContext);
   return (
     <div>
       <h1>HomePage</h1>
-      <h2>{`Welcome ${user?.name}`}</h2>
+      <h2>{`Welcome ${user.name}`}</h2>
     </div>
   );
 }
--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,9 +1,6 @@
 import React = require('react');
 
-// import { createContext, useContext, useState } from 'react';
 import { createContext, useState, useMemo } from 'react';
-
-// import axios from 'axios';
 
 export type AuthUser = {
   id: number,
@@ -23,45 +20,12 @@ type UserContextProviderProps = {
 export const UserContext = createContext({} as UserContextType);
 
 export function UserContextProvider({ children }: UserContextProviderProps) {
-  // this is where UserContext is getting value of user, is not being updated,
-  // if a premade user obj and not null - app has context of premade user
   const [user, setUser] = useState<AuthUser | null>(null);
 
   const userState = useMemo(() => ({ user, setUser }), [user]);
   return (
     <UserContext.Provider value={userState}>
-      {/* <UserContext.Provider value={{ user, setUser }}> */}
       {children}
     </UserContext.Provider>
   );
 }
-
-// // export const useUser = () => useContext(UserContext);
-// export function UserContextProvider({ children }: UserContextProviderProps) {
-//   const [user, setUser] = useState<AuthUser | null>(null);
-
-//   // const login = () => {
-//   //   // setUser(user);
-//   //   axios.get('/user')
-//   //     .then(({ data }: { data: object }) => {
-//   //       console.log(data);
-//   //       if (typeof data === 'object') {
-//   //         const curUser = { ...data };
-//   //         setUser(curUser);
-//   //       }
-//   //       // console.log(user);
-//   //     })
-//   //     .catch((err: Error) => console.error('failed setting user', err));
-//   // };
-
-//   // const logout = () => {
-//   //   setUser(null);
-//   // };
-
-//   return (
-//     <UserContext.Provider value={{ user, setUser }}>
-//       {/* <UserContext.Provider value={{ user, login, logout }}> */}
-//       {children}
-//     </UserContext.Provider>
-//   );
-// }
