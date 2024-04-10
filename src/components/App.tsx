f6243de7396b45bf3fa3ad82c58a3c685194f101
peytono <pcopcoeco@gmail.com>
2024-04-10 15:09:05 -0500

    navbar conditionally renders + clean up app.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,6 @@
 import React = require('react');
 
-// import { useContext, useEffect } from 'react';
-
-// import axios from 'axios';
-
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import HomePage from './HomePage';
 import Navbar from './Navbar';
 import Profile from './Profile';
@@ -14,39 +10,14 @@ import Events from './Events';
 import Resources from './Resources';
 import Login from './Login';
 
-// const UserContext = createContext(null);
 import { UserContextProvider } from '../context/UserContext';
 
 function App() {
-  // const [user, setUser] = useState({});
-
-  // useEffect(() => {
-  //   axios
-  //     .get('/user')
-  //     .then(({ data }: { data: object }) => {
-  //       console.log(data);
-  //       if (typeof data === 'object') {
-  //         const curUser = { ...data };
-  //         setUser(curUser);
-  //       }
-  //       // console.log(user);
-  //     })
-  //     .catch((err: Error) => console.error('failed setting user', err));
-  // }, []);
-  // getUser();
-
-  // const { user, login, logout } = useUser();
-
   return (
     <UserContextProvider>
-      {/* <> */}
-      {/* <UserContext.Provider value={user}> */}
-
-      {/* if path is index, we don't want to show navbar */}
-      <Navbar />
+      {(useLocation().pathname === '/') ? <div /> : <Navbar />}
       <Routes>
         <Route index element={<Login />} />
-        {/* <Route path="/home" element={<HomePage user={user} />} /> */}
         <Route path="/home" element={<HomePage />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/buddy" element={<BuddyChat />} />
@@ -54,9 +25,6 @@ function App() {
         <Route path="/events" element={<Events />} />
         <Route path="/resources" element={<Resources />} />
       </Routes>
-      {/* </UserContext.Provider> */}
-
-      {/* </> */}
     </UserContextProvider>
   );
 }
