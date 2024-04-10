f303a90fec5b7fca1c929a1d295093ad98f0a8f3
peytono <pcopcoeco@gmail.com>
2024-04-10 14:16:10 -0500

    setUser context in home

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,15 +1,26 @@
 import React = require('react');
 
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { UserContext } from '../context/UserContext';
 
 function HomePage() {
   const userContext = useContext(UserContext);
+
+  const { setUser, user } = userContext;
+
+  useEffect(() => {
+    setUser({
+      id: 1,
+      name: 'Peyton',
+      googleId: '1234',
+    });
+  }, [setUser]);
+
   console.log(userContext);
   return (
     <div>
       <h1>HomePage</h1>
-      <h2>{`Welcome ${userContext?.user?.name}`}</h2>
+      <h2>{`Welcome ${user?.name}`}</h2>
     </div>
   );
 }
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -6,18 +6,19 @@ import { UserContext } from '../context/UserContext';
 function Login() {
   const userContext = useContext(UserContext);
   console.log(userContext);
-  const handleLogin = () => {
-    userContext.setUser({
-      id: 1,
-      name: 'Peyton',
-      googleId: '1234',
-    });
-  };
+  // const handleLogin = () => {
+  //   userContext.setUser({
+  //     id: 1,
+  //     name: 'Peyton',
+  //     googleId: '1234',
+  //   });
+  // };
   return (
     <div>
       <h1>Google Login</h1>
       <form action="/auth/google" method="GET">
-        <button type="submit" onClick={handleLogin}>GOOGLE BUTTON</button>
+        <button type="submit">GOOGLE BUTTON</button>
+        {/* <button type="submit" onClick={handleLogin}>GOOGLE BUTTON</button> */}
       </form>
     </div>
   );
