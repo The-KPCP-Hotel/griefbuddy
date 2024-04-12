61b9f6cd56deda0a5388acc158f23f8e7de08884
peytono <pcopcoeco@gmail.com>
2024-04-12 17:28:19 -0500

    browser closes after finding correct response url

--- server/routes/events.ts
+++ server/routes/events.ts
@@ -1,13 +1,9 @@
-import express, {
-  Request, Response,
-} from 'express';
+import express, { Request, Response } from 'express';
 
-import axios, {
-  AxiosResponse,
-} from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 // import cheerio from 'cheerio';
-import puppeteer from 'puppeteer';
+import puppeteer, { HTTPResponse } from 'puppeteer';
 
 const events = express.Router();
 
@@ -16,24 +12,48 @@ events.get('/', async (req: Request, res: Response) => {
 
   const page = await browser.newPage();
 
-  await page.goto('https://www.neworleans.com/events/upcoming-events/?skip=0&categoryid=40&startDate=04%2F11%2F2024&endDate=05%2F11%2F2024&sort=title');
+  try {
+    await page.goto(
+      'https://www.neworleans.com/events/upcoming-events/?skip=0&categoryid=40&startDate=04%2F11%2F2024&endDate=05%2F11%2F2024&sort=title',
+    );
 
-  await page.evaluate(() => {
-    const data = document.body.innerHTML;
-    console.log(data);
-  });
+    // await page.evaluate(() => {
+    //   const data = document.body.innerHTML;
+    //   console.log(data);
+    // });
 
-  // page.on('request', (request) => {
-  //   console.log('request.url', request.url());
-  // });
+    // await page.on('request', (request) => {
+    //   console.log('request.url', request.url());
+    // });
 
-  await page.on('response', (response) => {
-    // contains events json
-    console.log('response.url', response.url());
-  });
+    await page.on('response', (response: HTTPResponse) => {
+      if (response.url().includes('find')) {
+        console.log('response.url', response.url());
+        return response.url();
+      }
+      // contains events json -- but not on every call - happening less and less?
+      // console.log('response.url', response.url());
+      // console.log('in on');
 
-  await browser.close();
-  res.sendStatus(200);
+      // response.json()
+      //   .then((jsonResponse) => {
+      //     console.log(jsonResponse.data);
+      //   })
+      //   .catch((err) => console.error('failed getting json', err));
+      // console.log(json);
+    });
+  } catch (err) {
+    console.error('failed getting json', err);
+  } finally {
+    console.log('before close'); // this is logging before the try block
+    // await browser.close(); // if not getting wanting response this might be stopping it
+    setTimeout(() => {
+      browser.close();
+      console.log('after set timeout and close');
+      res.sendStatus(200);
+    }, 3000);
+    // res.sendStatus(200);
+  }
 });
 
 export = events;
