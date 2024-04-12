fab65db838ba0c6c32998952681b79c5016ffb15
peytono <pcopcoeco@gmail.com>
2024-04-11 19:08:34 -0500

    GET events notes and change imports

--- server/routes/events.ts
+++ server/routes/events.ts
@@ -2,11 +2,13 @@ import express, {
   Request, Response,
 } from 'express';
 
-const cheerio = require('cheerio');
+import axios, {
+  AxiosResponse,
+} from 'axios';
+
+import cheerio from 'cheerio';
 
 const router = express.Router();
-
-const axios = require('axios');
 
 router.get('/', (req: Request, res: Response) => {
   axios
@@ -14,28 +16,41 @@ router.get('/', (req: Request, res: Response) => {
       // 'https://www.neworleans.com/events/upcoming-events/?skip=0&categoryid=40%2C34%2C123&startDate=04%2F10%2F2024&endDate=05%2F10%2F2024&sort=date',
       'https://www.neworleans.com/events/upcoming-events/?skip=0&categoryid=40&startDate=04%2F11%2F2024&endDate=05%2F11%2F2024&sort=title',
     )
-    .then((response: { data: [] }) => {
+    .then((response: AxiosResponse) => {
       // console.log(response.data);
 
       // Use Cheerio to parse the HTML
       const $ = cheerio.load(response.data);
-      // cannot send the cheerio variable
-      // console.log($('div.results-wrapper')['0'].children);
-      // const $results = $('div.results');
-      // console.log($results.html());
-      const $sharedItems = $('div.shared-items-container');
-      // console.log($sharedItems.html());
-      // console.log($('div.container'));
-      // console.log($('.container')['1']);
-      // console.log($('div.data-seo-event'));
-      // console.log($('div.shared-items').children());
-      // console.log($('li.dates'));
-      // console.log($('div.shared-items').find('li.dates'));
-      // console.log($('[data-type=event]').html());
-      // console.log($('div[data-type="event"]').html());
 
-      res.send($sharedItems.html());
-      // res.send($results.html());
+      // console.log($('div.results-wrapper')['0'].children); // this is not near nested enough
+
+      // const $results = $('div.results'); // this div is less nested than $sharedItems
+      // console.log($results.html());
+
+      const $sharedItems = $('div.shared-items-container');
+      // console.log($sharedItems.html()); // div.shared-item => div.container => empty
+
+      // console.log($('div.container'));
+      // console.log($('div.container').html()); // empty line
+
+      // console.log($('.container')['1']);
+      // console.log($('.container').html()); // empty line
+
+      // console.log($('div.data-seo-event'));
+      // console.log($('div.data-seo-event').html()); // null
+
+      // console.log($('div.shared-items').find('li.dates').html()); // null
+
+      // <h3>Map Results</h3><div class="map-counter" data-sv-map-items-counter=""></div>
+      // console.log($('div.shared-items').children().html());
+
+      // console.log($('li.dates').html()); // null
+
+      // console.log($('[data-type=event]').html()); // null
+      // console.log($('div[data-type="event"]').html()); // null
+
+      // res.send($sharedItems.html());
+      res.send($.html()); // the divs with info seem to be loaded in from a script and unavailable
     })
     .catch((err: Error) => {
       console.error('failed events scraping', err);
