79037b3927a17d28ea11533f6a763b55daf5258a
peytono <pcopcoeco@gmail.com>
2024-04-13 11:39:36 -0500

    Today's Events renders only today's events

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -5,23 +5,32 @@ import EventListItem from './EventsComponents/EventListItem';
 
 function Events() {
   const [events, setEvents] = useState([]);
+  const [eventsToday, setEventsToday] = useState([]);
 
   useEffect(() => {
     axios
       .get('/events/all')
       .then(({ data }) => {
         setEvents(data);
+        const today = new Date().toISOString();
+        console.log(today);
+        const curEvents = data.filter(
+          (event: { date: String }) => event.date.slice(0, 10) === today.slice(0, 10),
+        );
+        setEventsToday(curEvents);
       })
       .catch();
-  }, [events]);
+  }, []);
 
   return (
     <div>
       <Link to="/home">Home</Link>
       <h1>Events Page</h1>
       <ul>
-        {'Today\'s Events'}
-        {events.map((event) => <EventListItem event={event} />)}
+        Today&apos;s Events
+        {eventsToday.map((event) => (
+          <EventListItem key={event.OgId} event={event} />
+        ))}
       </ul>
     </div>
   );
