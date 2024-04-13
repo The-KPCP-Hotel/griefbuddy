d6a4608aa42c4d05361495e8faac0ac51e4301af
student <student@Carins-MacBook-Pro.local>
2024-04-13 17:12:14 -0500

    update User schema

--- server/routes/profile.ts
+++ server/routes/profile.ts
@@ -1,11 +1,25 @@
-const express = require('express');
+import * as express from 'express';
+import  {Request, Response } from 'express'
 const router = express.Router();
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
-router.get('/profile/user', (req, res) => {
-    
 
+router.get('/profile/user', (req: Request, res: Response) => {
+    let userr = prisma.User.findMany()
+    res.send(userr)
+})
+
+router.patch('/profile/user', (req: Request, res: Response) => {
+    const { googleId, location, currMood } = req.body
+    res.send({
+        where: {
+            googleId: googleId
+        },
+        data: {
+            
+        }
+    })
 })
 
 
