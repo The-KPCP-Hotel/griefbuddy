604fae340c2e1e9b61c29f81f2023f80af6b3b0a
peytono <pcopcoeco@gmail.com>
2024-04-12 19:40:19 -0500

    events router cleanup

--- server/routes/events.ts
+++ server/routes/events.ts
@@ -1,9 +1,12 @@
-import express, { Request, Response } from 'express';
+import express, {
+  Request, Response,
+} from 'express';
 
-import axios, { AxiosResponse } from 'axios';
+// import axios, { AxiosResponse } from 'axios';
 
-// import cheerio from 'cheerio';
-import puppeteer, { HTTPResponse } from 'puppeteer';
+import puppeteer, {
+  HTTPResponse,
+} from 'puppeteer';
 
 const events = express.Router();
 
@@ -12,52 +15,31 @@ events.get('/', async (req: Request, res: Response) => {
 
   const page = await browser.newPage();
 
-  // let eventsData: Array<Object>;
-  // try {
   await page.goto(
     'https://www.neworleans.com/events/upcoming-events/?skip=0&categoryid=40&startDate=04%2F11%2F2024&endDate=05%2F11%2F2024&sort=title',
   );
 
-  // eventsData =
-  await page.on('response', (response: HTTPResponse) => {
+  function pageOnResponse(response: HTTPResponse): undefined {
     if (response.url().includes('find')) {
-      console.log('response.url', response.url());
-      // return response.json().then((jsonResponse) => console.log(jsonResponse));
-      return response
+      // console.log('response.url', response.url());
+      response
         .json()
         .then(({ docs: { docs } }) => docs)
-        .then((docs) => {
-          setTimeout(() => {
-            browser.close();
-            console.log('after set timeout and close inside then block');
-            res.send(docs);
-            // res.sendStatus(200);
-          }, 3000);
+        .then((eventsJSON) => {
+          // this previously needed to be in a setTimeout to not run prematurely
+          browser.close();
+          res.send(eventsJSON);
+        })
+        .catch((err) => {
+          console.error('failed getting events json', err);
+          browser.close();
+          res.sendStatus(500);
         });
     }
-    // contains events json -- but not on every call - happening less and less?
-    // console.log('response.url', response.url());
-    // console.log('in on');
+  }
 
-    // response.json()
-    //   .then((jsonResponse) => {
-    //     console.log(jsonResponse.data);
-    //   })
-    //   .catch((err) => console.error('failed getting json', err));
-    // console.log(json);
-  });
-  // } catch (err) {
-  //   console.error('failed getting json', err);
-  // } finally {
-  //   console.log('before close'); // this is logging before the try block
-  //   // await browser.close(); // if not getting wanting response this might be stopping it
-  //   setTimeout(() => {
-  //     browser.close();
-  //     console.log('after set timeout and close');
-  //     // res.send(eventsData);
-  //     res.sendStatus(200);
-  //   }, 3000);
-  // }
+  await page.on('response', pageOnResponse);
 });
 
+// have not yet been able to find the proper export solution
 export = events;
