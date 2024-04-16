0ec78bfe3a9c56f0c9c56a871f764491faf75f78
peytono <pcopcoeco@gmail.com>
2024-04-16 17:30:35 -0500

    sending a message to chatbot logs ai response to client

--- server/helpers/openai-test.ts
+++ server/helpers/openai-test.ts
@@ -1,15 +1,35 @@
 import OpenAI from 'openai';
 
-const openai = new OpenAI();
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY, // This is the default and can be omitted
+});
+
+// type OpenaiMessageType = {
+//   role: string;
+//   content: string;
+// };
 
 async function main() {
-  const completion = await openai.chat.completions.create({
+  const params: OpenAI.Chat.ChatCompletionCreateParams = {
     messages: [{ role: 'system', content: 'You are an inquisitive friend to users who are grieving.' }],
-    model: 'gpt-3.5-turbo-1106',
-  });
-
-  console.log(completion.choices[0]);
-  return completion.choices[0];
+    model: 'gpt-3.5-turbo',
+  };
+  const chatCompletion: OpenAI.Chat.ChatCompletion = await openai.chat.completions.create(params);
+  console.log(chatCompletion.choices[0]);
+  return chatCompletion.choices[0];
 }
 
 export default main;
+
+// // async function main(messages: OpenaiMessageType[]) {
+// async function main(messages: ChatCompletionMessageParam[]) {
+//   const completion = await openai.chat.completions.create({
+//     messages,
+//     model: 'gpt-3.5-turbo-1106',
+//   });
+
+//   console.log(completion.choices[0]);
+//   return completion.choices[0];
+// }
+
+// export default main;
--- server/routes/chatbot.ts
+++ server/routes/chatbot.ts
@@ -1,16 +1,28 @@
 import express, { Request, Response } from 'express';
+import OpenAI from 'openai';
 import main from '../helpers/openai-test';
 
 const chatbot = express.Router();
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY, // This is the default and can be omitted
+});
+
+// const messageStart = [{ role: 'system', content: 'You are an inquisitive friend to users who are grieving.' }];
 
 chatbot.get('/new', async (req: Request, res: Response) => {
   const newChat = await main();
   res.send(newChat);
 });
 
-chatbot.post('/', (req: Request, res: Response) => {
+chatbot.post('/', async (req: Request, res: Response) => {
   console.log(req.body);
-  res.send(req.body);
+  const params: OpenAI.Chat.ChatCompletionCreateParams = {
+    messages: req.body.messages,
+    model: 'gpt-3.5-turbo',
+  };
+  const chatCompletion: OpenAI.Chat.ChatCompletion = await openai.chat.completions.create(params);
+  res.send(chatCompletion.choices[0]);
 });
 
 export = chatbot;
