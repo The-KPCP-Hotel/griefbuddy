05fa8cd6fa7d637f979f2fa5b0da30e39c6fea2d
peytono <pcopcoeco@gmail.com>
2024-04-15 21:39:53 -0500

    add input that when sent renders

--- src/components/ChatBot.tsx
+++ src/components/ChatBot.tsx
@@ -1,13 +1,53 @@
-import React = require('react');
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import {
+  ChakraProvider,
+  Heading,
+  Center,
+  StackDivider,
+  Input,
+  Stack,
+  Container,
+  Text,
+  Button,
+  HStack,
+} from '@chakra-ui/react';
 
 function ChatBot() {
+  const [message, setMessage] = useState('');
+
+  const [messages, addMessage] = useState([] as string[]);
+
+  const onChange = (e: { target: { value: string } }) => {
+    const { value } = e.target;
+    setMessage(value);
+  };
+
+  const onSend = () => {
+    const newMessages = messages.slice();
+    newMessages.push(message);
+    addMessage(newMessages);
+    setMessage('');
+  };
 
   return (
-    <div>
-      <Link to="/home" style={{fontSize: "55px"}}>⌂</Link>
-      <h1>Chat Bot</h1>
-    </div>
+    <ChakraProvider>
+      <Center>
+        <Heading size="3xl" color="blue.200">
+          Chat Bot
+        </Heading>
+      </Center>
+      <Container>
+        <Stack divider={<StackDivider />}>
+          {messages.map((text) => (
+            <Text>{text}</Text>
+          ))}
+          <HStack>
+            <Input onChange={onChange} value={message} />
+            <Button onClick={onSend}>Send</Button>
+          </HStack>
+        </Stack>
+      </Container>
+    </ChakraProvider>
   );
 }
 
