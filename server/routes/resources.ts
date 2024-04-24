778cfdef247019daac4cf3af95de9cc3e46247fd
student <student@Carins-MacBook-Pro.local>
2024-04-23 19:57:05 -0500

    add resources get and post routes

--- server/routes/resources.ts
+++ server/routes/resources.ts
@@ -6,8 +6,33 @@ const prisma = new PrismaClient()
 
 
 router.get('/allResources', (req: Request, res: Response) => {
-    
+    prisma.Resource.findMany()
+    .then((results: any) => {
+        res.send(results).status(200)
+    })
+    .catch((err: string) => {
+        console.error(err)
+        res.sendStatus(500)
+    })
 })
 
+
+router.post('/addResource', (req: Request, res: Response) => {
+    const { type, url } = req.body.data
+    prisma.Resource.create({
+        data: {
+            type,
+            url
+        }   
+    })
+    .then(() => {
+        console.log('Resource successfully added')
+        res.sendStatus(200)
+    })
+    .catch((err: string) => {
+        console.error(err)
+        res.sendStatus(500)
+    })
+})
 
 module.exports = router;
