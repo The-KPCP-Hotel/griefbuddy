12e99454a616d19c310cfb48a75768704069e13d
peytono <pcopcoeco@gmail.com>
2024-05-07 15:40:38 -0500

    create basic bot info component

---
+++ src/components/BotComponents/Info.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from '@chakra-ui/react';
+
+function Info() {
+  return (
+    <Text marginBottom="30px" marginLeft="30px" justifyContent="center">
+      Welcome to the Chat Bot! Here you can get things off your chest or have any other private
+      conversation you&apos;d like.
+    </Text>
+  );
+}
+
+export default Info;
