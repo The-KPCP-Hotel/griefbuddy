05e2e699a153f5d0fc9042327b1078afaf58ad1b
peytono <pcopcoeco@gmail.com>
2024-05-14 16:37:27 -0500

    add google icon to login button

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import { Button, ChakraProvider, Heading, Center } from '@chakra-ui/react';
+import { Button, ChakraProvider, Heading, Center, Image } from '@chakra-ui/react';
 
 function Login() {
   return (
     <ChakraProvider>
       <Center>
-        <Heading>Google Login</Heading>
+        <Heading size="3xl" color="blue.600">GriefBuddy</Heading>
       </Center>
-      <Center>
+      <Center m="20px">
         <form action="/auth/google" method="GET">
-          <Button type="submit">GOOGLE BUTTON</Button>
+          <Button
+            type="submit"
+            rightIcon={(
+              <Image
+                padding="5px"
+                width="40px"
+                src="https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA"
+              />
+            )}
+          >
+            Sign in with Google
+          </Button>
         </form>
       </Center>
     </ChakraProvider>
