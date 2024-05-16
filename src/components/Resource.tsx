3335aada631297af7d1a134c7932c5842b6da73e
peytono <pcopcoeco@gmail.com>
2024-05-16 13:43:18 -0500

    remove deleted component

--- src/components/Resource.tsx
+++ src/components/Resource.tsx
@@ -8,12 +8,10 @@ import {
   OrderedList,
   Container,
 } from '@chakra-ui/react';
-// import Breadcrumbs from './NavComponents/Breadcrumbs';
 
 function Resource() {
   return (
     <ChakraProvider>
-      {/* <Breadcrumbs type="resources" /> */}
       <Center>
         <Heading size="3xl" color="blue.200" marginBottom="35px">
           Resource
