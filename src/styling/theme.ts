0f817e062c162b3ffb1a36b2cbc46578fc43f451
peytono <pcopcoeco@gmail.com>
2024-05-21 16:14:48 -0500

    Revert "change initial color mode to system"
    This reverts commit 4ecc1111abe2546d5fc58a2dfd9decae5e1b29bb.

--- src/styling/theme.ts
+++ src/styling/theme.ts
@@ -1,7 +1,7 @@
 import { extendTheme, ThemeConfig, Colors } from '@chakra-ui/react';
 
 const config: ThemeConfig = {
-  initialColorMode: 'system',
+  initialColorMode: 'dark',
   useSystemColorMode: false,
 };
 
