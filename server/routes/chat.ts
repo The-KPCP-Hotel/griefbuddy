b402f9c25c7364074e7cf092acf5e373fc37750e
peytono <pcopcoeco@gmail.com>
2024-06-11 18:29:40 -0500

    GET to chat/user sends array of all users

--- server/routes/chat.ts
+++ server/routes/chat.ts
@@ -1,9 +1,18 @@
 import express from 'express';
 
+// having issues getting everything from one import
+import { User as UserType } from '@prisma/client';
+
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+const { User } = prisma;
+
 const chat = express.Router();
 
-chat.get('/user', (req, res) => {
-  res.send('success');
+chat.get('/user', async (req, res) => {
+  const searchedUsers: UserType[] = await User.findMany();
+  res.send(searchedUsers);
 });
 
 export = chat;
