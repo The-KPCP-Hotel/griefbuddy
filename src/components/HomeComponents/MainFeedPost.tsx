8046fb7e3bfe03c562f0d0fdca26a230cd75ee52
student <student@Carins-MacBook-Pro.local>
2024-06-26 07:14:15 -0500

    UPDATE post delete button color to grey

--- src/components/HomeComponents/MainFeedPost.tsx
+++ src/components/HomeComponents/MainFeedPost.tsx
@@ -150,7 +150,7 @@ function MainFeedPost(props: any) {
           variant="ghost"
           colorScheme="gray"
           aria-label="See menu"
-          icon={<VscTrash />}
+          icon={<VscTrash color='grey'/>}
           onClick={() => {
             deletePost();
           }}
@@ -205,11 +205,11 @@ function MainFeedPost(props: any) {
       <CardBody>
         <Text>{text}</Text>
       </CardBody>
-      <Image
+      {/* <Image
         objectFit="cover"
         src="https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
         alt="Chakra UI"
-      />
+      /> */}
       <Center>
         <InputGroup>
           <Input
