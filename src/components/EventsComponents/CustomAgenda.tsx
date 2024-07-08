10becb92eb445cdef62dbe2cd609e83ae37af957
peytono <pcopcoeco@gmail.com>
2024-07-08 14:57:06 -0500

    remove CustomAgenda errors through line 180

--- src/components/EventsComponents/CustomAgenda.tsx
+++ src/components/EventsComponents/CustomAgenda.tsx
@@ -6,13 +6,20 @@ import React, { useRef, useEffect } from 'react';
 // import scrollbarSize from 'dom-helpers/scrollbarSize';
 
 // import { navigate } from './utils/constants';
-import { Event as EventType, Navigate as navigate } from 'react-big-calendar';
+import {
+  Event as EventType,
+  Navigate as navigate,
+  Messages,
+  DateLocalizerSpec,
+  CalendarProps,
+} from 'react-big-calendar';
 // import { inRange } from './utils/eventLevels';
 // import { inRange } from 'react-big-calendar';
 // import { isSelected } from './utils/selection';
 import isEqual from 'lodash/isEqual';
+// import { /* eq, */ inRange } from 'date-arithmetic/index';
 
-function inRange(e: any, start: any, end: any, accessors: any, localizer: any) {
+function inRange(e: any, start: Date, end: Date, accessors: any, localizer: any) {
   const event = {
     start: accessors.start(e),
     end: accessors.end(e),
@@ -47,28 +54,38 @@ function Agenda({
 }: {
   accessors: {
     title: (event: EventType) => {};
-    end: (event: EventType) => {};
-    start: (event: EventType) => {};
+    end: (event: EventType) => Date | undefined;
+    start: (event: EventType) => Date | undefined;
+    allDay: (event: EventType) => boolean;
   };
-  components: { event: EventType; date: any };
+  // components: { event: EventType; date: Date; time: any };
+  components: { event: any; date: any; time: any };
   date: Date;
   events: EventType[];
   getters: {
-    eventProp: (
-      event: EventType,
-      start: object,
-      end: object,
-      isSelected: boolean,
-    ) => {};
+    eventProp: (event: EventType, start: object, end: object, isSelected: boolean) => {};
   };
   length: number;
   localizer: {
-    startOf: (day: Date, lib: string) => {};
-    endOf: (day: Date, lib: string) => {};
-    format: (day: Date, lib: string) => {};
+    // startOf: (day: Date, lib: string) => {};
+    startOf: DateLocalizerSpec['startOf'];
+    // endOf: (day: Date, lib: string) => {};
+    endOf: DateLocalizerSpec['endOf'];
+    // format: (day: Date, lib: string) => {};
+    // format: (value: FormatInput, format: string, culture?: Culture) => string;
+    format: DateLocalizerSpec['format'];
+    messages: Messages;
+    // eq: (date: Date, date2: Date, unit?: Unit) => boolean;
+    eq: DateLocalizerSpec['eq'];
+    isSameDate: DateLocalizerSpec['isSameDate'];
+    gt: DateLocalizerSpec['gt'];
+    lt: DateLocalizerSpec['lt'];
   };
-  onDoubleClickEvent: () => {};
-  onSelectEvent: () => {};
+  // onDoubleClickEvent: () => {};
+  onDoubleClickEvent: CalendarProps['onDoubleClickEvent'];
+  // onSelectEvent?: ((event: EventType, e: React.SyntheticEvent<HTMLElement>) => void) | undefined;
+  // onSelectEvent: () => {};
+  onSelectEvent: CalendarProps['onSelectEvent'];
   selected: object;
 }) {
   const headerRef = useRef(null);
@@ -81,46 +98,7 @@ function Agenda({
   //   _adjustHeader();
   // });
 
-  const renderDay = (day: Date, eventsParam: EventType[], dayKey: string) => {
-    const { event: Event, date: AgendaDate } = components;
-
-    const eventsRender = eventsParam.filter((e) =>
-      inRange(e, localizer.startOf(day, 'day'), localizer.endOf(day, 'day'), accessors, localizer));
-
-    return eventsRender.map((event, idx) => {
-      const title = accessors.title(event);
-      const end = accessors.end(event);
-      const start = accessors.start(event);
-
-      const userProps = getters.eventProp(event, start, end, isSelected(event, selected));
-
-      const dateLabel = idx === 0 && localizer.format(day, 'agendaDateFormat');
-      const first =
-        idx === 0 ? (
-          <td rowSpan={events.length} className="rbc-agenda-date-cell">
-            {AgendaDate ? <AgendaDate day={day} label={dateLabel} /> : dateLabel}
-          </td>
-        ) : (
-          false
-        );
-
-      return (
-        <tr key={`${dayKey}_${idx}`} className={userProps.className} style={userProps.style}>
-          {first}
-          <td className="rbc-agenda-time-cell">{timeRangeLabel(day, event)}</td>
-          <td
-            className="rbc-agenda-event-cell"
-            onClick={(e) => onSelectEvent && onSelectEvent(event, e)}
-            onDoubleClick={(e) => onDoubleClickEvent && onDoubleClickEvent(event, e)}
-          >
-            {Event ? <Event event={event} title={title} /> : title}
-          </td>
-        </tr>
-      );
-    }, []);
-  };
-
-  const timeRangeLabel = (day, event) => {
+  const timeRangeLabel = (day: Date, event: EventType) => {
     let labelClass = '';
     const TimeComponent = components.time;
     let label = localizer.messages.allDay;
@@ -132,7 +110,9 @@ function Agenda({
       if (localizer.eq(start, end)) {
         label = localizer.format(start, 'agendaTimeFormat');
       } else if (localizer.isSameDate(start, end)) {
-        label = localizer.format({ start, end }, 'agendaTimeRangeFormat');
+        // using imported format as type - first argument does not match format's typing
+        // below line has not been changing from react-big-calendar source
+        // label = localizer.format({ start, end }, 'agendaTimeRangeFormat');
       } else if (localizer.isSameDate(day, start)) {
         label = localizer.format(start, 'agendaTimeFormat');
       } else if (localizer.isSameDate(day, end)) {
@@ -148,6 +128,56 @@ function Agenda({
         {TimeComponent ? <TimeComponent event={event} day={day} label={label} /> : label}
       </span>
     );
+  };
+
+  const renderDay = (day: Date, eventsParam: EventType[], dayKey: string) => {
+    const { event: Event, date: AgendaDate } = components;
+
+    const eventsRender = eventsParam.filter((e) =>
+      inRange(e, localizer.startOf(day, 'day'), localizer.endOf(day, 'day'), accessors, localizer));
+
+    return eventsRender.map((event, idx) => {
+      const title = accessors.title(event);
+      const end = accessors.end(event);
+      const start = accessors.start(event);
+
+      type UserProps = {
+        className?: string;
+        style?: React.CSSProperties;
+      };
+
+      const userProps: UserProps = getters.eventProp(
+        event,
+        start,
+        end,
+        isSelected(event, selected),
+      );
+
+      const dateLabel = idx === 0 && localizer.format(day, 'agendaDateFormat');
+      const first =
+        idx === 0 ? (
+          <td rowSpan={events.length} className="rbc-agenda-date-cell">
+            {`${AgendaDate ? <AgendaDate day={day} label={dateLabel} /> : dateLabel}`}
+          </td>
+        ) : (
+          false
+        );
+
+      return (
+        // eslint-disable-next-line react/no-array-index-key
+        <tr key={`${dayKey}_${idx}`} className={userProps.className} style={userProps.style}>
+          {first}
+          <td className="rbc-agenda-time-cell">{timeRangeLabel(day, event)}</td>
+          <td
+            className="rbc-agenda-event-cell"
+            onClick={(e) => onSelectEvent && onSelectEvent(event, e)}
+            onDoubleClick={(e) => onDoubleClickEvent && onDoubleClickEvent(event, e)}
+          >
+            {`${Event ? <Event event={event} title={title} /> : title}`}
+          </td>
+        </tr>
+      );
+    }, []);
   };
 
   const _adjustHeader = () => {
