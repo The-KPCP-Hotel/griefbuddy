904e2e8da175ecb788ba72e47317444cd02a6df3
student <student@Carins-MacBook-Pro.local>
2024-07-08 15:53:14 -0500

    receiving resources correctly from webscraper

--- server/routes/resources.ts
+++ server/routes/resources.ts
@@ -19,7 +19,7 @@ router.get('/allResources', (req: Request, res: Response) => {
 
 
 router.get('/addResource', async (req: Request, res: Response) => {
-
+    console.log('hitting addResource')
     puppeteer.launch().then(async function(browser) {
         const page = await browser.newPage();
         await page.goto('https://www.lifebanc.org/resources/for-families/online-grief-resources/');
@@ -38,9 +38,73 @@ router.get('/addResource', async (req: Request, res: Response) => {
         })
 
         const allResources: any = await page.evaluate(() => {
-            return Array.from(document.querySelectorAll('.panel-right .bg-powderblue'), (e: HTMLElement) => (e.innerHTML))
+            // resourceTitles.forEach((title) => {
+            //     if()
+            // })
+            const allElements = Array.from(document.querySelector('.panel-right .bg-powderblue').children)
+            type ResourceLink = {
+                name: string;
+                url: string;
+                description?: string
+            }
+            type ResourceObj = {
+                title?: string;
+                resources?: ResourceLink []
+            }
+            const resourceObjects: ResourceObj[] = []
+            let currObj: ResourceObj = {}
+            let currLink = {} as ResourceLink
+            //when we reach an h3, start a new obj
+            allElements.map((e: HTMLElement) => {
+                console.log(e.tagName)
+                if(e.tagName === 'H2'){
+                    return
+                }
+                if(e.tagName === 'H3'){
+                   
+                        resourceObjects.push(currObj)
+                        currObj = {
+                            title: e.innerText,
+                            resources: []
+                        }
+                    
+                    return 
+                }
+                if(!currObj.resources){
+                    return
+                }
+                const link = e.querySelector('a')                
+                if(link){
+                    if(Object.keys(currLink).length){
+                        currObj.resources.push(currLink)
+                        currLink = {} as typeof currLink
+                    }
+                    currLink.url = link.href
+                    currLink.name = link.innerText
+                    return
+                }
+                if(!currLink.description && e.innerText){
+                    currLink.description = e.innerText
+                    currObj.resources.push(currLink)
+                    currLink = {} as typeof currLink
+                    
+                }
+                return e.outerHTML
+            })
+            if(Object.keys(currLink).length){
+                currObj.resources.push(currLink)
+            }
+            if(Object.keys(currObj).length){
+                resourceObjects.push(currObj)
+            }
+            return resourceObjects
+            // return Array.from(document.querySelectorAll('.panel-right .bg-powderblue'), (e: HTMLElement) => (e.innerHTML))
         })
-
+        // const allResources: any = await page.evaluate(() => {
+        //     return Array.from(document.querySelectorAll('.panel-right .bg-powderblue'), (e: HTMLElement) => (e.innerHTML))
+        // })
+        res.send(allResources)
+        return
         const resourcesSplit = allResources[0].replace('\n', '').trim().split('<h3>')
         const resourceStorage: String[] = [] 
         resourcesSplit.forEach((resource: any) => {
