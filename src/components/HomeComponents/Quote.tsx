3cbfb2ee8a4deb9894dab1da16709f089497fd49
peytono <pcopcoeco@gmail.com>
2024-07-09 12:15:00 -0500

    only display quotes when screen is larger than base size

--- src/components/HomeComponents/Quote.tsx
+++ src/components/HomeComponents/Quote.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Center, Text, Box } from '@chakra-ui/react';
+import { Button, Center, Text, Box, Flex, Spacer, useMediaQuery } from '@chakra-ui/react';
 import { ArrowRightIcon } from '@chakra-ui/icons';
 import axios from 'axios';
 
@@ -11,6 +11,8 @@ function Quote({ userId }: { userId: number }) {
   };
 
   const [quote, setQuote] = useState({} as Quote);
+
+  const [isLargerThanBase] = useMediaQuery('(min-width: 30em)');
 
   const getQuote = () => {
     axios
@@ -35,24 +37,45 @@ function Quote({ userId }: { userId: number }) {
 
   return (
     <>
-      {quote.quote ? (
+      {quote.quote && isLargerThanBase ? (
         <Center>
           <Box>
             <Text fontStyle="italic">{quote.quote}</Text>
-            <Text>{`  -${quote.author}`}</Text>
+            {/* <Text>
+              {`  -${quote.author}`}
+              <Button
+                type="button"
+                onClick={blockQuote}
+                variant="link"
+                p=".5rem"
+              >
+                Don&apos;t show this quote again
+              </Button>
+            </Text> */}
+            <Flex maxW="100%">
+              <Box minW="fit-content" p=".5rem">
+                <Text>{`  -${quote.author}`}</Text>
+              </Box>
+              <Spacer />
+              <Box p=".5rem">
+                <Button type="button" onClick={blockQuote} variant="link" p="0">
+                  Don&apos;t show this quote again
+                </Button>
+              </Box>
+            </Flex>
           </Box>
           <Button ml="1rem" variant="ghost" onClick={getQuote}>
             <ArrowRightIcon />
           </Button>
         </Center>
       ) : null}
-      {quote.quote ? (
+      {/* {quote.quote ? (
         <Center m="20px">
           <Button type="button" onClick={blockQuote}>
             Don&apos;t show this quote again
           </Button>
         </Center>
-      ) : null}
+      ) : null} */}
     </>
   );
 }
