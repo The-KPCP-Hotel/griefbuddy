52180fd8705ba053822ac979ad1b8d1a540db7fa
peytono <pcopcoeco@gmail.com>
2024-07-10 12:44:09 -0500

    add larger mb to chat icon

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -254,7 +254,7 @@ function Chat() {
 
   return (
     <Container>
-      <Center mt=".5rem" mb=".5rem">
+      <Center mt=".5rem" mb=".75rem">
         <Heading color={color} as="h2" aria-label="Chat" boxSize="3rem">
           <ChatIcon aria-label="Chat icon" boxSize="2.75rem" />
         </Heading>
