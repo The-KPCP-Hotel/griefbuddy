899dfea82a7c17754108c52842f7bc56d982a9c3
peytono <pcopcoeco@gmail.com>
2024-07-14 13:51:05 -0500

    match buddy component theme to rest of app

--- src/components/BuddyComponents/Buddy.tsx
+++ src/components/BuddyComponents/Buddy.tsx
@@ -1,14 +1,18 @@
-import { Container, Heading, Text } from '@chakra-ui/react';
 import React from 'react';
+import { Center, Container, Heading, Text, useColorModeValue } from '@chakra-ui/react';
 // will eventually need socket, but not there yet
 // import { Socket } from 'socket.io';
 
 function Buddy() {
-// function Buddy({ socket }: { socket: Socket }) {
+  // function Buddy({ socket }: { socket: Socket }) {
+
+  const color = useColorModeValue('blue.600', 'blue.200');
   return (
     <Container>
-      <Heading>Buddy</Heading>
-      <Text>
+      <Center>
+        <Heading color={color}>Buddy</Heading>
+      </Center>
+      <Text textAlign="center" color={color} fontWeight="bold">
         This is the Beta Buddy chat. You&apos;ll be temporarily paired with another user, and
         you&apos;re encouraged to give extra support to one another while making new buddies.
       </Text>
